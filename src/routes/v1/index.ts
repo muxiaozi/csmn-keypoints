138aa3b00968f50b7ebbc7e20a0e8b777fa91ce2
huhongwei <hongwei.hu@foxmail.com>
2025-12-24 11:01:21 +0800

    feat: 添加听悟回调

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,13 @@
 import express, { Router } from "express";
 import deviceRoutes from "./device.js";
+import tingwuRoutes from "./tingwu.js";
 
 const router = Router();
 
 router.use("/uploads", express.static("uploads"));
 
 router.use("/devices", deviceRoutes);
+
+router.use("/tingwu", tingwuRoutes);
 
 export default router;
---
+++ src/routes/v1/tingwu.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response } from "express";
+import { prisma } from "../../app.js";
+import {
+  failed,
+  forbidden,
+  success,
+  unauthorized,
+} from "../../utils/response.js";
+import { asyncHandler } from "../../middlewares/error_handler.js";
+import {
+  validateBody,
+  validateParams,
+  validateQuery,
+} from "../../middlewares/validation.js";
+import { deviceSchemas, commonSchemas, schemas } from "../../utils/schemas.js";
+import logger from "../../utils/logger.js";
+
+const router = Router();
+
+router.post(
+  "/callback",
+  asyncHandler(async (req: Request, res: Response) => {
+    logger.info("Receive body:", JSON.stringify(req.body));
+    logger.info("Receive query", JSON.stringify(req.query));
+    return success(res, {});
+  })
+);
+
+export default router;
